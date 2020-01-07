d68c46f7dd5546defc457cf8acbad0a8aa58b6c7
Anton Mikhailov <anton@mikhailov.center>
2020-01-07 20:42:13 +0300

    models import and export with theirs initial data

--- saraswati/mooncal/admin.py
+++ saraswati/mooncal/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Ritual, MoonDay
 
-admin.site.register(Ritual)
-admin.site.register(MoonDay)
+class RitualResource(resources.ModelResource):
+    class Meta:
+        model = Ritual
+
+class MoonDayResource(resources.ModelResource):
+    class Meta:
+        model = MoonDay
+
+
+class RitualAdmin(ImportExportModelAdmin):
+    resource_class = RitualResource
+
+class MoonDayAdmin(ImportExportModelAdmin):
+    resource_class = MoonDayResource
+
+admin.site.register(Ritual, RitualAdmin)
+admin.site.register(MoonDay, MoonDayAdmin)
+
+# admin.site.register(Ritual)
+# admin.site.register(MoonDay)
 

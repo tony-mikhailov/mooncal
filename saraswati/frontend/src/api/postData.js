6e98fe43a9e8fb7707778d1ef9cde474e2d66983
Kirill Vanyukov <vanyukovkirill@gmail.com>
2020-02-16 15:56:02 +0300

    change hural

---
+++ saraswati/frontend/src/api/postData.js
@@ -0,0 +1,35 @@
+import makeRequest from './helpers/makeRequest';
+
+export function postDateData(date, params) {
+    return (
+        makeRequest(`${date.year}/${date.month}/${date.day}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        })
+    )
+
+}
+
+export function postMonthData(date, params) {
+    return (
+        makeRequest(`${date.year}/${date.month}`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(params)
+        })
+    );
+}
+
+export function postHurals(date) {
+    return (
+        makeRequest(`hurals`)
+    );
+}
+

f63a2939e983165bacd7bfd75b1f36dab03da60f
Anton Mikhailov <anton@mikhailov.center>
2020-02-24 21:52:03 +0300

    serialize day

--- saraswati/mooncal/serializers.py
+++ saraswati/mooncal/serializers.py
@@ -16,13 +16,14 @@ class MoonDaySerializer(serializers.ModelSerializer):
     weekday = serializers.SerializerMethodField('get_weekday_from_moonday')
     date = serializers.SerializerMethodField('get_date_from_moonday')
     month = serializers.SerializerMethodField('get_month_from_moonday')
+    day = serializers.SerializerMethodField('get_day_from_moonday')
 
     morning_hural_id = serializers.SerializerMethodField('get_morning_hural_from_moonday')
     day_hural_id = serializers.SerializerMethodField('get_day_hural_from_moonday')
     
     class Meta:
         model = MoonDay
-        fields = ['year','day_no','moon_day_no','morning_hural_id','day_hural_id','url','weekday','date','month','baldjinima','dashinima','tersuud','modon_hohimoy','riha','pagshag','good_for_haircut','good_for_travel','significant_day','comment','article_link','lamas_checked',]
+        fields = ['year','month','day','day_no','moon_day_no','morning_hural_id','day_hural_id','url','weekday','date','month','baldjinima','dashinima','tersuud','modon_hohimoy','riha','pagshag','good_for_haircut','good_for_travel','significant_day','comment','article_link','lamas_checked',]
         
     def get_url_from_moonday(self, moonday):
         return moonday.url()
@@ -35,6 +36,9 @@ class MoonDaySerializer(serializers.ModelSerializer):
 
     def get_month_from_moonday(self, moonday):
         return moonday.month()
+
+    def get_day_from_moonday(self, moonday):
+        return moonday.day()
 
 
     def get_morning_hural_from_moonday(self, moonday):

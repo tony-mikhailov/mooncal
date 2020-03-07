010e1f22d4f13e126b7c172683f60b2e24256aae
Kirill Vanyukov <vanyukovkirill@gmail.com>
2020-03-07 17:56:26 +0300

    редактор месяца - день недели

--- saraswati/frontend/src/containers/Month/MonthLine/index.js
+++ saraswati/frontend/src/containers/Month/MonthLine/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import style from './monthLine.module.css';
-import HuralSelect from '~/components/huralSelect/index';
-import { Link } from 'react-router-dom';
+import HuralSelect from '~/components/huralSelect';
+import dayWeek from '~/store/dayWeek';
 
 
 export default function MonthLine(props) {
@@ -24,12 +25,12 @@ export default function MonthLine(props) {
         }
 
         return (
-            <li key={item.day}>
-                <span className={style.day}>
+            <li key={item.day} className={item.weekday == 7 ? 'mb-3' : '' }>
+                <span className={style.day + ' p-2'}>
                     <Link to={`/${item.year}/${item.month}/${item.day}`} className=''>{item.day}</Link>
                 </span>
-                <span className={style.moon_day_no}>
-                    {item.moon_day_no}
+                <span className={style.day_week + ' p-2'}>
+                    {dayWeek[item.weekday]}
                 </span>
                 <span className={style.morning_hural}>
                     <HuralSelect
@@ -45,6 +46,9 @@ export default function MonthLine(props) {
                         selectedId={item.day_hural_id}
                     />
                 </span>
+                <span className={style.moon_day_no + ' p-2'}>
+                    {item.moon_day_no}
+                </span>
             </li>
         )
     })
@@ -55,14 +59,17 @@ export default function MonthLine(props) {
                 <span className={style.day}>
                     Число
                 </span>
-                <span className={style.moon_day_no}>
-                    Лунный день
+                <span className={style.day_week}>
+                    День недели
                 </span>
                 <span className={style.morning_hural}>
                     1 хурал
                 </span>
                 <span className={style.day_hural}>
                     2 хурал
+                </span>
+                <span className={style.moon_day_no}>
+                    Лунный день
                 </span>
             </li>
 
